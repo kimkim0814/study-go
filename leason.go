f47fa7e8a5e5686eddf8ceefead92e6a8f31128d
木村　翼 <tnryj505@gmail.com>
2022-10-25 11:08:52 +0900

    Initial commit

---
+++ leason.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//定数
+
+const (
+	URL      = "http://xxx.co.jp"
+	SiteName = "test"
+)
+
+const (
+	A = 1
+	B
+	C
+	D = "A"
+	E
+	F
+)
+
+const (
+	c0 = iota
+	c1
+	c2
+)
+func main() {
+	// fmt.Println(Pi)
+
+	// Pi = 3
+	// fmt.Println(Pi)
+
+	fmt.Println(URL, SiteName)
+
+	fmt.Println(A, B, C, D, E, F)
+
+	fmt.Println(c0,c1,c2)
+}
