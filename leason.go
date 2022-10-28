ba4e1a9ce83ae9f4644515e9ce33ec4e7063cb7b
木村　翼 <tnryj505@gmail.com>
2022-10-28 10:57:57 +0900

    for文

--- leason.go
+++ leason.go
@@ -2,28 +2,54 @@ package main
 
 import "fmt"
 
-//関数
-//ジェネレーター
-
-func integers() func() int{
-	i := 0
-	return func() int {
-		i ++ 
-		return i
-	}
-}
+//for
 
 func main() {
-	ints := integers()
+	// i := 0
+	// for {
+	// 	i++
+	// 	if i == 3{
+	// 		break
+	// 	}
+	// 	fmt.Println("Loop")
+	// }
 
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
+	// point := 0
+	// for point < 0 {
+	// 	fmt.Println(point)
+	// 	point ++
+	// }
 
-	otherints := integers()
-	fmt.Println(otherints())
-	fmt.Println(otherints())
-	fmt.Println(otherints())
+	// for i := 0; i < 10;i++{
+	// 	if i == 3{
+	// 		continue
+	// 	}
+	// 	if i == 6 {
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
 
+	//PHPのforeachみたいな使い方ができる
+	// arr := [3]int{1,2,3}
+	// for i:=0; i< len(arr); i++ {
+	// 	fmt.Println(arr[i])
+	// }
+
+	// arr := [3]int{1,2,3}
+
+	// for _,v := range arr{
+	// 	fmt.Println(v)
+	// }
+
+	// sl := []string{"Python", "PHP", "GO"}
+	// for i, v := range sl {
+	// 	fmt.Println(i, v)
+	// }
+
+	m := map[string]int{"appple":100,"banana":200}
+
+	for k,v := range m{
+		fmt.Println(k,v)
+	}
 }
