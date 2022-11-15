72a0f564f89c0851871f55c9dc58638384ddd8e6
木村　翼 <tnryj505@gmail.com>
2022-11-15 11:29:12 +0900

    スライス　copy

--- leason.go
+++ leason.go
@@ -5,34 +5,25 @@ import (
 )
 
 //スライス
-//append make len cap
+//copy
 
 func main() {
-	sl := []int{100,200}
-	fmt.Println(sl)
+	// sl := []int{100, 200}
+	// sl2 := sl
 
-	sl = append(sl,300)
-	fmt.Println(sl)
-	
-	sl = append(sl,400,500,600)
-	fmt.Println(sl)
+	// sl2[0] = 1000
 
-	sl2 := make([]int,5)
+	// fmt.Println(sl)
+
+	// var i int = 10
+	// i2 := i
+	// i2 = 100
+	// fmt.Println(i,i2)
+
+	sl := []int{1,2,3,4,5}
+	sl2 := make([]int,5,10)
 	fmt.Println(sl2)
+	n := copy(sl2,sl)
 
-	fmt.Println(len(sl2))
-
-	fmt.Println(cap(sl2))
-
-	sl3 := make([]int,5,10)
-
-	fmt.Println(len(sl3))
-
-	fmt.Println(cap(sl3))
-
-	sl3= append(sl3,1,2,3,4,5,6)
-
-	fmt.Println(len(sl3))
-
-	fmt.Println(cap(sl3))
+	fmt.Println(n,sl2)
 }
