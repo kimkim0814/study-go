9c8adc36714cbd3389e93a371f81de51d14d54a9
木村　翼 <tnryj505@gmail.com>
2022-11-18 11:20:55 +0900

    スライスfor

--- leason.go
+++ leason.go
@@ -5,25 +5,17 @@ import (
 )
 
 //スライス
-//copy
+//for
 
 func main() {
-	// sl := []int{100, 200}
-	// sl2 := sl
+	sl := []string{"A","B","C"}
+	fmt.Println(sl)
 
-	// sl2[0] = 1000
+	// for i := range sl{
+	// 	fmt.Println(i)
+	// }
 
-	// fmt.Println(sl)
-
-	// var i int = 10
-	// i2 := i
-	// i2 = 100
-	// fmt.Println(i,i2)
-
-	sl := []int{1,2,3,4,5}
-	sl2 := make([]int,5,10)
-	fmt.Println(sl2)
-	n := copy(sl2,sl)
-
-	fmt.Println(n,sl2)
+	for i := 0; i < len(sl);i++{
+		fmt.Println(sl[i])
+	}
 }
