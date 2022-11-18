0dc9ced5e51d4e117c9fe68734ec14dbe520b936
木村　翼 <tnryj505@gmail.com>
2022-11-18 11:28:44 +0900

    スライス　可変長引数

--- leason.go
+++ leason.go
@@ -5,17 +5,26 @@ import (
 )
 
 //スライス
-//for
+//可変長引数
+
+func Sum(s ...int) int {
+	n := 0
+	for _, v:= range s{
+		n += v
+	}
+	return n
+}
+
 
 func main() {
-	sl := []string{"A","B","C"}
-	fmt.Println(sl)
+	fmt.Println(Sum(1,2,3))
 
-	// for i := range sl{
-	// 	fmt.Println(i)
-	// }
+	fmt.Println(Sum(1,2,3,4,5,6,7,8,9,10))
 
-	for i := 0; i < len(sl);i++{
-		fmt.Println(sl[i])
-	}
+	fmt.Println(Sum())
+
+	sl := []int{1,2,3}
+
+	fmt.Println(Sum(sl...))
+
 }
